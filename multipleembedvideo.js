78637699e03c6f8712094a080f0ef0d181f8a7ac
fardinshah <58081665+vidclouds@users.noreply.github.com>
2019-11-22 21:40:23 +0600

    Create multipleembedvideo.js

---
+++ multipleembedvideo.js
@@ -0,0 +1,5 @@
+<script type='text/javascript'>//<![CDATA[
+// Script Multiple Embed video - Don't Edit
+$(document).ready(function(){(function(){var g=anime.length;var e=$(".--tab-eps");for(var h=0;h<g;h++){e.append("<a class='ganti-eps' data-id='"+h+"' data-eps='Episode "+(h+1)+"'>"+(h+1)+"</a>")}var f=$(".--show-video");$(".ganti-eps").click(function(){f.html('<div class="--responsive"><iframe src="'+anime[parseInt($(this).data("id"))]+'" allowfullscreen="true" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" width="100%" height="360"  ></iframe></div>');$("#--title-anime").text($(this).data("eps"))})}())});
+//]]>
+</script>
