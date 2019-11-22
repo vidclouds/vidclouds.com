fef89b9f9a27d90dda07de32e6f9dd67aeea1fc9
fardinshah <58081665+vidclouds@users.noreply.github.com>
2019-11-22 22:06:30 +0600

    Create rating-widget.js

---
+++ rating-widget.js
@@ -0,0 +1,5 @@
+<script type='text/javascript'>
+//<![CDATA[
+!function(e,t,n,i){window.RW_Async_Init=function(){RW.init({huid:"444860",uid:"3a5bc2bfd9ba3816217836ab08ce88e1",source:"website",options:{"advanced": {"layout": {"lineHeight": "20px"}},style:"oxygen",isDummy:!1}}),RW.render()};var s,a=e.getElementsByTagName(n)[0],c="rw-js",r=e.location,o="Y"+t.getFullYear()+"M"+t.getMonth()+"D"+t.getDate(),d=r.protocol,g=r.search.indexOf("DBG=")>-1?"":".min",l="https:"==d?"secure."+i+"js/":"js."+i;e.getElementById(c)||((s=e.createElement(n)).id=c,s.async=!0,s.type="text/javascript",s.src=d+"//"+l+"external"+g+".js?ck="+o,a.parentNode.insertBefore(s,a))}(document,new Date,"script","rating-widget.com/");
+//]]>
+</script>
